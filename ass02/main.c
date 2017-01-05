3776ef58519273b9720b02b223c6c6e22f85ef4f
Peter Verschuure <peterverschuure@hotmail.com>
2017-01-05 18:10:04 +0100

    Add additional tests for assignment 2 (there is still a bug in the assembly function multiply)

--- ass02/main.c
+++ ass02/main.c
@@ -8,5 +8,24 @@ int main(void)
 {
     unsigned long long int m = multiply(0, 0);
     printf("Result of multiply(0, 0) = %ld\n", m);
+
+    m = multiply(1, 0);
+	printf("Result of multiply(1, 0) = %ld\n", m);
+
+	m = multiply(0, 1);
+	printf("Result of multiply(0, 1) = %ld\n", m);
+
+	m = multiply(1, 1);
+	printf("Result of multiply(1, 1) = %ld\n", m);
+
+	m = multiply(1, 4294967295);
+	printf("Result of multiply(1, 4294967295) = %ld\n", m);
+
+	m = multiply(4294967295, 1);
+	printf("Result of multiply(4294967295, 1) = %ld\n", m);
+
+	m = multiply(4294967295, 4294967295);
+	printf("Result of multiply(4294967295, 4294967295) = %ld\n", m);
+
     return 0;
 }
