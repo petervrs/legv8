52be36c2e6c0732527d93248437fe6393672badb
Peter Verschuure <peterverschuure@hotmail.com>
2017-04-07 19:32:16 +0200

    Fix assignment 2, update assignment 3

--- ass02/main.c
+++ ass02/main.c
@@ -7,25 +7,25 @@ extern unsigned long long int multiply(unsigned int a, unsigned int b);
 int main(void)
 {
     unsigned long long int m = multiply(0, 0);
-    printf("Result of multiply(0, 0) = %ld\n", m);
+    printf("Result of multiply(0, 0) = %llu\n", m);
 
     m = multiply(1, 0);
-	printf("Result of multiply(1, 0) = %ld\n", m);
+	printf("Result of multiply(1, 0) = %llu\n", m);
 
 	m = multiply(0, 1);
-	printf("Result of multiply(0, 1) = %ld\n", m);
+	printf("Result of multiply(0, 1) = %llu\n", m);
 
 	m = multiply(1, 1);
-	printf("Result of multiply(1, 1) = %ld\n", m);
+	printf("Result of multiply(1, 1) = %llu\n", m);
 
 	m = multiply(1, 4294967295);
-	printf("Result of multiply(1, 4294967295) = %ld\n", m);
+	printf("Result of multiply(1, 4294967295) = %llu\n", m);
 
 	m = multiply(4294967295, 1);
-	printf("Result of multiply(4294967295, 1) = %ld\n", m);
+	printf("Result of multiply(4294967295, 1) = %llu\n", m);
 
 	m = multiply(4294967295, 4294967295);
-	printf("Result of multiply(4294967295, 4294967295) = %ld\n", m);
+	printf("Result of multiply(4294967295, 4294967295) = %llu\n", m);
 
     return 0;
 }
